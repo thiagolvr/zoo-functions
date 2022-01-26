51e0475a4ec9f3013aac97ae7431ca35c03f61b5
Thiago Oliveira <thiago_marcos_@hotmail.com>
2022-01-26 17:53:36 -0300

    req 1 completado

--- src/getSpeciesByIds.js
+++ src/getSpeciesByIds.js
@@ -1,7 +1,8 @@
-const data = require("../data/zoo_data");
+const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function getSpeciesByIds(ids) {
-  // vamos começar
+function getSpeciesByIds(...ids) {
+  return species.filter(({ id }) => ids.find((elementId) => elementId === id));
 }
 
 module.exports = getSpeciesByIds;
