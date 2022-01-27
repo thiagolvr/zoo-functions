18b440d3fdf9245f7eeef3d748783aa08553c8fa
Thiago Oliveira <thiago_marcos_@hotmail.com>
2022-01-27 16:53:15 -0300

    req 4 completado

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,29 @@
 const data = require('../data/zoo_data');
 
+const { employees } = data;
+
 function isManager(id) {
-  // seu código aqui
+  const findManager = employees.find((item) => item.managers.includes(id));
+  if (findManager) {
+    return true;
+  }
+  return false;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  const validateManager = isManager(managerId);
+  if (!validateManager) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  } {
+    const catNames = employees.filter((item) => item.managers.includes(managerId));
+    return catNames.map((item) => `${item.firstName} ${item.lastName}`);
+  }
 }
 
 module.exports = { isManager, getRelatedEmployees };
+
+/* REFERÊNCIA
+Lógica de solução baseado na lógica da aluna Juliana Espinola:
+https://github.com/tryber/sd-19-c-project-zoo-functions/pull/120/files
+
+*/
