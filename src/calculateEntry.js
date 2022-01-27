537241aceb2627a4ee7bf8a5ff1edaad2c552f92
Thiago Oliveira <thiago_marcos_@hotmail.com>
2022-01-27 16:03:34 -0300

    req 6 completado

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,27 @@
 const data = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const objCountAges = { child: 0, adult: 0, senior: 0 };
+  entrants.forEach((item) => {
+    if (item.age < 18) {
+      objCountAges.child += 1;
+    } else if (item.age >= 18 && item.age < 50) {
+      objCountAges.adult += 1;
+    } else {
+      objCountAges.senior += 1;
+    }
+  });
+  return objCountAges;
 }
 
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || !entrants.length) {
+    return 0;
+  }
+  const numbersOfEntrants = countEntrants(entrants);
+  const cust = Object.keys(numbersOfEntrants).reduce((acc, item) => acc
+  + numbersOfEntrants[item] * data.prices[item], 0);
+  return cust;
 }
 
 module.exports = { calculateEntry, countEntrants };
--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -3,7 +3,7 @@ const data = require('../data/zoo_data');
 const { employees } = data;
 
 function getEmployeeByName(employeeName) {
-  if (employeeName === '' || employeeName === undefined) {
+  if (!employeeName) {
     return {};
   }
   return employees.find(({ firstName, lastName }) => firstName === employeeName
