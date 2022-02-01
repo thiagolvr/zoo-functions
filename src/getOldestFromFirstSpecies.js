280a5b38c3fc00a2ec362df04c2f1f6eee53caa5
Thiago Oliveira <thiago_marcos_@hotmail.com>
2022-02-01 18:07:43 -0300

    req 9 completado

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,14 @@
 const data = require('../data/zoo_data');
 
-function getOldestFromFirstSpecies(id) {
-  // seu código aqui
-}
+const { employees } = data;
+const { species } = data;
+
+const getOldestFromFirstSpecies = (idParam) => {
+  const findEmployee = employees.find(({ id }) => id === idParam).responsibleFor[0];
+  const findResidents = species.find(({ id }) => id === findEmployee).residents;
+  const olderAge = findResidents.sort((a, b) => b.age - a.age);
+  const result = olderAge.map(({ name, sex, age }) => [name, sex, age])[0];
+  return result;
+};
 
 module.exports = getOldestFromFirstSpecies;
